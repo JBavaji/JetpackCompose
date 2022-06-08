e383b1ef71a1c51b9bcef68c5c989b5b32563225
Jayeshgiri Bavaji <jbavaji@gmail.com>
2022-06-08 19:16:08 +0530

    add: Composable Row

--- JetBizCard/app/src/main/java/com/jbavaji/jetbizcard/MainActivity.kt
+++ JetBizCard/app/src/main/java/com/jbavaji/jetbizcard/MainActivity.kt
@@ -3,41 +3,57 @@ package com.jbavaji.jetbizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.jbavaji.jetbizcard.ui.theme.JetBizCardTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetBizCardTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    Surface(
+        color = Color.DarkGray,
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            HorizontalColoredBar(Color.Magenta)
+            HorizontalColoredBar(Color.Red)
+            HorizontalColoredBar(Color.Green)
+        }
+    }
+}
+
+@Composable
+fun HorizontalColoredBar(color: Color) {
+    Surface(
+        color = color,
+        modifier = Modifier.size(60.dp, 600.dp)
+    ) {
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    JetBizCardTheme {
-        Greeting("Android")
-    }
+fun MainScreenPreview() {
+    MainScreen()
 }
